1f26e21d5194cf7f2f8c615af05a5c3cc8440c66
Jani Nikula <jani@nikula.org>
2021-01-29 20:52:28 +0200

    update-to-0.7

--- doc/conf.py
+++ doc/conf.py
@@ -12,11 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
 
-import ext
+from hawkmoth.util import readthedocs
 
-ext.clang_setup()
+readthedocs.clang_setup()
 
 # print(os.environ.get('LD_LIBRARY_PATH'))
 
